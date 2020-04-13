aa8ee706af4a1581876c8774cf1c599026bdec8d
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-04-13 06:45:08 +0300

    Ported semaphore queue for WINAPI

--- lab3.cpp
+++ lab3.cpp
@@ -1,4 +1,7 @@
 #include "lab3.h"
+#include <windows.h>
+
+#include <vector>
 
 //
 // lab3 code should be located here!
@@ -23,3 +26,54 @@ int lab3_init()
 {
     return 0;
 }
+
+//Class SemaphoreQueue ported for windows
+class SemaphoreQueue {
+private:
+    std::vector<HANDLE> m_semaphores;
+
+public:
+    SemaphoreQueue(size_t size)
+    {
+        m_semaphores.resize(size);
+        m_semaphores[0] = createSem(1);
+        for (auto it = m_semaphores.begin() + 1; it != m_semaphores.end(); it++) {
+            *it = createSem(0);
+        }
+    }
+
+    ~SemaphoreQueue()
+    {
+        for (auto& s : m_semaphores) {
+            CloseHandle(s);
+        }
+        m_semaphores.clear();
+    }
+
+    void in(size_t id)
+    {
+        while (WaitForSingleObject(m_semaphores[id], 0) != WAIT_OBJECT_0)
+            ;
+    }
+
+    void out(size_t id)
+    {
+        bool atEnd = (id == (m_semaphores.size() - 1));
+        if (atEnd) {
+            ReleaseSemaphore(&m_semaphores[0], 1, NULL);
+        } else {
+            ReleaseSemaphore(&m_semaphores[id + 1], 1, NULL);
+        }
+    }
+
+private:
+    HANDLE createSem(int init_val)
+    {
+        HANDLE out = CreateSemaphore(nullptr, init_val, 1, nullptr); // Max counter is 1. Queue doesn't need more.
+        if (out)
+            return out;
+
+        std::cerr << "Cannot create semaphore, code:" << GetLastError() << std::endl;
+        exit(-1);
+    }
+};
